698c8110591d88b99e2128091bef0ea5c99ea2be
BabciaDokuczliwa <oblaczynska@gmail.com>
2022-07-05 14:57:08 +0200

    mat-language

---
+++ solution.js
@@ -0,0 +1,16 @@
+console.log(Math.min(-1,4));
+console.log(Math.max(-1,4));
+
+console.log(Math.ceil(3321.32321));
+console.log(Math.ceil(326.76));
+console.log(Math.ceil(-9.78));
+console.log(Math.ceil(43.342));
+console.log(Math.ceil(76788.7));
+
+console.log(Math.floor(3321.32321));
+console.log(Math.floor(326.76));
+console.log(Math.floor(-9.78));
+console.log(Math.floor(28.329));
+console.log(Math.floor(76788.7));
+
+console.log(Math.ceil(Math.random() * 6));
